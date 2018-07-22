e99109dc59126327da83fcecb9ee7b8a5b51cace
raistlinmolina <raistlinmolina@gmail.com>
2018-07-22 19:17:41 +0200

    First commit.

---
+++ PuckShotMeter/sloeber.ino.cpp
@@ -0,0 +1,19 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-07-22 19:11:22
+
+#include "Arduino.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
+
+void dmpDataReady() ;
+void setup() ;
+void loop() ;
+
+#include "PuckShotMeter.ino"
+
+
+#endif
