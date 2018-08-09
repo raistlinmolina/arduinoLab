e317154488df39779fbe7db25701c408f87ad0a5
raistlinmolina <raistlinmolina@gmail.com>
2018-08-09 23:51:02 +0200

    RemoteXY first Interface integrated and crude accel aprox shown on meters.

--- PuckShotMeter/PuckShotMeter.ino
+++ PuckShotMeter/PuckShotMeter.ino
@@ -8,6 +8,94 @@
 
 // Sketch based on the one from Jeff Rowberg.
 
+// RemoteXY select connection mode and include library
+#define REMOTEXY_MODE__ESP8266WIFI_LIB_POINT
+#include <ESP8266WiFi.h>
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings
+#define REMOTEXY_WIFI_SSID "Alfheim"
+#define REMOTEXY_WIFI_PASSWORD "JoSeMaNuElMoLiNa"
+#define REMOTEXY_SERVER_PORT 6377
+
+
+// RemoteXY configurate
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =
+  { 255,23,0,102,0,150,0,8,24,0,
+  66,130,75,6,22,17,2,26,1,0,
+  6,4,12,12,2,31,83,116,97,114,
+  116,0,7,36,21,15,50,6,2,26,
+  2,21,130,1,5,33,69,26,32,66,
+  130,8,43,13,10,2,26,66,130,24,
+  43,14,11,2,26,66,130,40,43,15,
+  12,2,26,66,130,56,43,17,13,2,
+  26,67,5,12,52,5,3,2,32,11,
+  67,5,28,53,6,3,2,32,11,67,
+  5,44,54,7,3,2,32,11,67,5,
+  61,55,7,3,2,32,11,67,5,81,
+  14,10,5,2,24,11,67,5,9,35,
+  62,5,2,32,21,67,4,21,8,44,
+  6,2,24,21,1,0,6,18,12,12,
+  2,31,83,97,118,101,0 };
+
+
+
+// this structure defines all the variables of your control interface
+struct {
+
+    // input variable
+  uint8_t StartButton; // =1 if button pressed, else =0
+  char playerNameField[21];  // string UTF8 end zero
+  uint8_t button_1; // =1 if button pressed, else =0
+
+    // output variable
+  int8_t currentShotLevel; // =0..100 level position
+  int8_t shot4Level; // =0..100 level position
+  int8_t shot3Level; // =0..100 level position
+  int8_t shot2Level; // =0..100 level position
+  int8_t shot1Level; // =0..100 level position
+  char shot4Text[11];  // string UTF8 end zero
+  char shot3Text[11];  // string UTF8 end zero
+  char shot2Text[11];  // string UTF8 end zero
+  char shot1Text[11];  // string UTF8 end zero
+  char currentShotText[11];  // string UTF8 end zero
+  char lastShotsText[21];  // string UTF8 end zero
+  char playerNameText[21];  // string UTF8 end zero
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0
+
+} RemoteXY;
+#pragma pack(pop)
+
+
+
+#define PIN_STARTBUTTON 13
+
+
+void setupRemoteXY(){
+	  RemoteXY_Init ();
+
+	  pinMode (PIN_STARTBUTTON, OUTPUT);
+	  RemoteXY.shot4Level = 99;
+	  RemoteXY.shot3Level = 75;
+	  RemoteXY.shot2Level = 50;
+	  RemoteXY.shot1Level = 25;
+	  strcpy(RemoteXY.playerNameText, "Jugador");
+
+	  // TODO you setup code
+
+}
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+
+
+
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
@@ -144,7 +232,7 @@ void initialize(){
 
 		// enable Arduino interrupt detection
 		Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-		attachInterrupt(D7, dmpDataReady, RISING);
+		attachInterrupt(D6, dmpDataReady, RISING);
 		mpuIntStatus = mpu.getIntStatus();
 
 		// set our DMP Ready flag so the main loop() function knows it's okay to use it
@@ -269,6 +357,10 @@ void readableWorldAccel(){
     aaWorldOld.x = aaWorld.x;
     aaWorldOld.y = aaWorld.y;
     aaWorldOld.z = aaWorld.z;
+    RemoteXY.shot1Level = abs(aaWorld.x)/100;
+    RemoteXY.shot2Level = abs(aaWorld.y)/100;
+    RemoteXY.shot3Level = abs(aaWorld.z)/100;
+
     }
 }
 #endif
@@ -279,13 +371,18 @@ void readableWorldAccel(){
 
 void setup() {
 
+	//RemoteXY
+
+	setupRemoteXY();
+	// configure LED for output
+	//pinMode(LED_PIN, OUTPUT);
+
+	//RemoteXY
+
 	Serial.begin(115200);
 	while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
 	initialize();
-
-	// configure LED for output
-	pinMode(LED_PIN, OUTPUT);
 }
 
 
@@ -295,6 +392,18 @@ void setup() {
 // ================================================================
 
 void loop() {
+
+	//RemoteXY
+
+	RemoteXY_Handler ();
+
+
+
+//	digitalWrite(PIN_STARTBUTTON, (RemoteXY.StartButton==0)?LOW:HIGH);
+
+	//RemoteXY
+
+
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
 
--- PuckShotMeter/sloeber.ino.cpp
+++ PuckShotMeter/sloeber.ino.cpp
@@ -2,13 +2,38 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-08-08 23:33:57
+//This file has been generated on 2018-08-09 23:44:51
 
 #include "Arduino.h"
+#define REMOTEXY_MODE__ESP8266WIFI_LIB_POINT
+#include <ESP8266WiFi.h>
+#include <RemoteXY.h>
+#define REMOTEXY_WIFI_SSID "Alfheim"
+#define REMOTEXY_WIFI_PASSWORD "JoSeMaNuElMoLiNa"
+#define REMOTEXY_SERVER_PORT 6377
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+extern const uint8_t RemoteXY_CONF_PROGMEM[];
+#define PIN_STARTBUTTON 13
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Wire.h"
 
+void setupRemoteXY();
 void dmpDataReady() ;
 void initialize();
 void readableYawPitchRoll();
