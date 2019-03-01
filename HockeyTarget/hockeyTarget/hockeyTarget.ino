2fb0c2e5cfafa68cd380568e3987e3870ae973b7
Jose Manuel Molina Pascual <jose.manuel.molina.pascual@ericsson.com>
2019-03-01 16:02:14 +0100

    New functions added

--- HockeyTarget/hockeyTarget/hockeyTarget.ino
+++ HockeyTarget/hockeyTarget/hockeyTarget.ino
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+//Times in seconds
 const int totalTargets = 5;
 int litTime = 10;
 int totalTime = 180;
 int changeTime = 10;
 int timeUsed = 0;
-int blinkTime = 3;
+int blinkTime = 1;
+const bool useDeMux = false;
+int lastLitTarget = 0;
+bool stillLit = false;
+int multiplier = 1000; //To convert to milliseconds
+int interval = 1000; // Interval
+int targetsLeft = totalTargets;
+
 
 enum litStatus { off, lit, blinking};
 
@@ -24,12 +32,14 @@ typedef struct {
 
 target targets[totalTargets];
 
-int targetsPins[totalTargets] = {0,0,0,0,0};
+int targetsPins[totalTargets] = {3,4,5,6,7};
 
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  initTargets();
 }
 
 void initTargets(){
@@ -45,28 +55,23 @@ void initTargets(){
 }
 
 void showStatus(){
+  Serial.println("#### TARGET STATUS ###");
+  
+  for (int i=0; i<totalTargets; i++){
+      Serial.print(itoa(i, NULL, 10));
+  }
+  
   for (int i=0; i<totalTargets; i++){
     if (targets[i].hit){
-      Serial.print("Target ");
-      Serial.print(itoa(i, NULL, 10));
-      Serial.println(" has been hit");
-    }
-    if (targets[i].lit == lit){
-      Serial.print("Target ");
-      Serial.print(itoa(i,NULL,10));
-      Serial.println(" is on");
-    }else if (targets[i].lit == off){
-      Serial.print("Target ");
-      Serial.print(itoa(i,NULL,10));
-      Serial.println(" is off");
+      Serial.print("H");
+    }else if (targets[i].lit == lit){
+      Serial.print("H");
     }else if (targets[i].lit == blinking){
-      Serial.print("Target ");
-      Serial.print(itoa(i,NULL,10));
-      Serial.println(" is off");
+      Serial.print("B");
+    }else if (targets[i].lit == off){
+      Serial.print("O");
     }else{
-      Serial.print("Target ");
-      Serial.print(itoa(i,NULL,10));
-      Serial.println(" has an invalid status.");
+      Serial.print("?");
     }
   }
 }
@@ -78,6 +83,50 @@ void updateTargets(){
 void updateTargetsStatus(){
 
 }
+
+void setTarget(int i){
+    targets[i].hit = false;
+    targets[i].timeLeft = changeTime * multiplier;
+    targets[i].lit = blinking;
+}
+
+void hitTarget(int i){
+    targets[i].hit = true;
+    targets[i].timeLeft = 0;
+    targets[i].lit = lit;
+    targetsLeft--;
+}
+
+int selectTarget(){
+  stillLit = false;
+  int newTarget = lastLitTarget;
+  for (int i = 0; i++; i<totalTargets){
+    if (targets[i].timeLeft > 0){
+      stillLit = true;
+      lastLitTarget = i;
+      Serial.print("Target ");
+      Serial.print(itoa(i,NULL,10);
+      Serial.print(" has still ");
+      Serial.print(itoa(targets[i].timeLeft);
+      Serial.println(" milliseconds left");
+    }
+  }
+  if (!stillLit){
+    //All targets are either hit or off
+    do{
+        newTarget = random(totalTargets);
+     }while(((newTarget != lastTarget) || (targetsLeft == 1))  && (!target[newTarget].hit))
+     //We check that the new target is different from the current one (unles it is the last one) and that it is not an already hit target. 
+    Serial.print("New target selected: ");
+    Serial.println(itoa(newTarget,NULL,10));
+    setTarget(newTarget);
+    updateTargets();
+    showStatus();
+  }
+  return newTarget;
+}
+
+
 
 
 void loop() {
